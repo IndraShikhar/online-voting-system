36ef1d99879bb38d91159097a2f73dd66a8c0abf
IndraShikhar <shikhar1575@gmail.com>
2025-11-10 17:45:44 +0530

    frontend setup

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline text-center mt-10">
+        A Online Voting System
+      </h1>
+    </div>
+  )
+}
+
+export default App
