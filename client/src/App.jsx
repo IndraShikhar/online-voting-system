ded076ec44c42d1fec7464a637928c5be2acb9aa
Aanand-Anupam <anupamaanandthakur@gmail.com>
2025-11-10 18:29:45 +0530

    initial commit by anupam

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ function App() {
   return (
     <div className="App">
       <h1 className="text-3xl font-bold underline text-center mt-10">
-        A Online Voting System
+        A Online Voting System -By anupam
       </h1>
     </div>
   )
