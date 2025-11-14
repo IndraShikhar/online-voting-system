7cb3f81238ba2d4edfcf9a848e738da4967e95cc
IndraShikhar <shikhar1575@gmail.com>
2025-11-14 17:36:56 +0530

    implemented election routes

--- server/routes/electionRoutes.js
+++ server/routes/electionRoutes.js
@@ -1,20 +1,19 @@
-import express from "express"
-import electionController from "../controllers/electionController.js"
+import express from 'express';
+import electionController from '../controllers/electionController.js';
+import authMiddleware from '../middleware/authMiddleware.js';
 const electionRouter = express.Router();
 
-electionRouter.post('/create', electionController.createNewElection)
+electionRouter.use(authMiddleware.protect);
 
-electionRouter.get('/', electionController.getAllElections)
+electionRouter.get('/', electionController.getAllElections);
+electionRouter.get('/:id', electionController.getDetailOfElection);
 
-electionRouter.get('/:id', electionController.getDetailOfElection)
+electionRouter.use(authMiddleware.restrictTo('admin'));
 
-electionRouter.patch('/start/:id', electionController.startElection)
-
-electionRouter.patch('/end/:id', electionController.endElection)
-
-electionRouter.patch('/declare-result/:id', electionController.declareResults)
-
-electionRouter.delete('/delete/:id', electionController.deleteElection)
-
+electionRouter.post('/create', electionController.createNewElection);
+electionRouter.patch('/start/:id', electionController.startElection);
+electionRouter.patch('/end/:id', electionController.endElection);
+electionRouter.patch('/declare-result/:id', electionController.declareResults);
+electionRouter.delete('/delete/:id', electionController.deleteElection);
 
 export default electionRouter;
