987474f13d855d1f33b43fe19cebfce7a7520976
IndraShikhar <shikhar1575@gmail.com>
2025-11-16 11:16:49 +0530

    new User Routes

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -6,7 +6,6 @@ import { isCorrectPassword, filterObj } from '../utils/utils.js';
 import { uploadBufferToCloudinary } from '../utils/uploadToCloudinary.js';
 
 const userController = {
-
   // ==============================
   // 1. GET ALL USERS
   // ==============================
@@ -28,13 +27,13 @@ const userController = {
   // 2. REGISTER USER
   // ==============================
   registerUser: catchAsync(async (req, res, next) => {
-    console.log("Calling register user method:");
+    console.log('Calling register user method:');
     console.log(req);
     const { username, name, email, password } = req.body;
 
-    
     // Insert into DB
-    let avatar_url = 'https://res.cloudinary.com/dn8ofrxix/image/upload/v1763201582/users_avatars/lbdmmsl8vd6boyzw68yk.jpg';
+    let avatar_url =
+      'https://res.cloudinary.com/dn8ofrxix/image/upload/v1763201582/users_avatars/lbdmmsl8vd6boyzw68yk.jpg';
     await db.query(
       `INSERT INTO users (username, name, email, password, avatar_url, role)
        VALUES (?, ?, ?, ?, ?, ?)`,
@@ -91,59 +90,57 @@ const userController = {
   // 5. UPDATE USER PROFILE
   // ==============================
   updateUserProfile: catchAsync(async (req, res, next) => {
-  console.log("BODY:", req.body);
-  console.log("FILE:", req.file);
+    console.log('BODY:', req.body);
+    console.log('FILE:', req.file);
 
-  // Stop password changes
-  if (req.body.password) {
-    return next(new AppError('This route is not for password updates.', 400));
-  }
-
-  const allowed = ['name', 'email'];
-  const filteredBody = filterObj(req.body, ...allowed);
-
-  let avatar_url ;
-
-  // File upload
-  if (req.file?.buffer) {
-    try {
-      
-      const result = await uploadBufferToCloudinary(req.file.buffer, {
-        folder: 'users_avatars',
-        resource_type: 'image',
-      });
-      avatar_url = result.secure_url;
-      filteredBody.avatar_url = avatar_url;
-    } catch (err) {
-      return next(new AppError('Avatar upload failed', 500));
+    // Stop password changes
+    if (req.body.password) {
+      return next(new AppError('This route is not for password updates.', 400));
     }
-  }
 
-  // Update user
-  await db.query('UPDATE users SET ? WHERE user_id = ?', [
-    filteredBody,
-    req.user.user_id,
-  ]);
+    const allowed = ['name', 'email'];
+    const filteredBody = filterObj(req.body, ...allowed);
 
-  // Return updated user
-  const [rows] = await db.query(
-    'SELECT user_id, username, email, name, avatar_url, role FROM users WHERE user_id = ?',
-    [req.user.user_id]
-  );
+    let avatar_url;
 
-  res.status(200).json({
-    status: 'success',
-    data: { user: rows[0] },
-  });
-}),
+    // File upload
+    if (req.file?.buffer) {
+      try {
+        const result = await uploadBufferToCloudinary(req.file.buffer, {
+          folder: 'users_avatars',
+          resource_type: 'image',
+        });
+        avatar_url = result.secure_url;
+        filteredBody.avatar_url = avatar_url;
+      } catch (err) {
+        return next(new AppError('Avatar upload failed', 500));
+      }
+    }
 
+    // Update user
+    await db.query('UPDATE users SET ? WHERE user_id = ?', [
+      filteredBody,
+      req.user.user_id,
+    ]);
+
+    // Return updated user
+    const [rows] = await db.query(
+      'SELECT user_id, username, email, name, avatar_url, role FROM users WHERE user_id = ?',
+      [req.user.user_id]
+    );
+
+    res.status(200).json({
+      status: 'success',
+      data: { user: rows[0] },
+    });
+  }),
 
   // ==============================
   // 6. BLOCK USER
   // ==============================
   blockUser: catchAsync(async (req, res, next) => {
     const username = req.params.username; // updated
-    const banned_by = req.user.username;  // updated
+    const banned_by = req.user.username; // updated
     const election_id = req.body.election_id || null;
     const reason = req.body.reason || null;
     const ban_type = election_id ? 'election' : 'permanent';
@@ -163,6 +160,46 @@ const userController = {
         election_id,
         reason,
         ban_type,
+      },
+    });
+  }),
+
+  unblockUser: catchAsync(async (req, res, next) => {
+    const userID = req.params.userId;
+
+    // Delete from bans
+    await db.query('DELETE FROM bans WHERE user_id = ?', [userID]);
+
+    const [user] = await db
+      .query('SELECT username FROM users WHERE user_id = ?', [userID])
+      .then((results) => results[0]);
+
+    user.password = undefined;
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        message: `User '${user.username}' has been unblocked.`,
+        user,
+      },
+    });
+  }),
+
+  deleteUser: catchAsync(async (req, res, next) => {
+    const userID = req.params.userId;
+
+    const [user] = await db
+      .query('SELECT username FROM users WHERE user_id = ?', [userID])
+      .then((results) => results[0]);
+
+    const username = user.username;
+    // Delete from users
+    await db.query('DELETE FROM users WHERE user_id = ?', [userID]);
+
+    res.status(204).json({
+      status: 'success',
+      data: {
+        message: `User '${username}' has been deleted.`,
       },
     });
   }),
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import userController from '../controllers/userController.js';
 import authMiddleware from '../middleware/authMiddleware.js';
-import upload from "../middleware/multer.js";
+import upload from '../middleware/multer.js';
 
 const userRouter = express.Router();
 
@@ -14,14 +14,15 @@ userRouter.use(authMiddleware.protect);
 userRouter.get('/profile', userController.getUserProfile);
 userRouter.put(
   '/profile',
-  upload.single('avatar'),   // 'avatar' is the key you send in Postman
+  upload.single('avatar'), // 'avatar' is the key you send in Postman
   userController.updateUserProfile
 );
-
 
 userRouter.use(authMiddleware.restrictTo('admin'));
 
 userRouter.get('/all', userController.getAllUsers);
 userRouter.patch('/block/:userId', userController.blockUser);
+userRouter.patch('/unblock/:userId', userController.unblockUser);
+userRouter.delete('/delete/:userId', userController.deleteUser);
 
 export default userRouter;
